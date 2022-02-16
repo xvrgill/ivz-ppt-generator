b5e8799eca3cfd261157b75d2b18bbe42fa362d3
xvrgill <60839017+xvrgill@users.noreply.github.com>
2022-02-16 05:26:36 -0500

    heroku fixes

--- api/resources/pptx.py
+++ api/resources/pptx.py
@@ -1,5 +1,5 @@
 """Resource that generates ppt from post group"""
-from api import app
+# from api import app
 from flask import send_file
 from flask_restful import Resource
 import requests
@@ -8,24 +8,26 @@ from typing import List, Optional
 from api.context_managers.ppt_cm import PPTContextManager
 from api.models.post_key_mappings import post_key_mappings as pkm
 from scripts.ppt_generator import PowerPointGenerator
+from api.resources.post_group import PostGroup
+from api.resources.post import Post
 
 
 class PPT(Resource):
     def get(self, id):
         ppt_data = dict()
-        root_url = app.config["ROOT_URL"]
-        post_group_url = f"{root_url}/post-group/{str(id)}"
-        post_group_response: requests.Response = requests.get(post_group_url)
-        post_group_data: dict = post_group_response.json()
+        # post_group_url = api.url_for(PostGroup, id=id)
+        post_group_response = PostGroup().get(id)
+        # print(post_group_response)
+        post_group_data = post_group_response.json
 
         ppt_data["group_name"] = post_group_data["fields"]["Name"]
         ppt_data["posts"] = list()
 
         for _id in post_group_data["fields"]["Linked Posts"]:
             # get data from post route
-            post_url = f"http://{root_url}/post/{str(_id)}"
-            post_response: requests.Response = requests.get(post_url)
-            post_data = post_response.json()
+            # post_url = f"http://{root_url}/post/{str(_id)}"
+            post_response = Post().get(_id)
+            post_data = post_response.json
             # key mappings to make them more pythonic and uniform
             updated_post_data = {(pkm[k] if k in pkm else k): v for k, v in post_data["fields"].items()}
             # append dictionary of post data
