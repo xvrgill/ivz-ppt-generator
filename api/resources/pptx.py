545f0e2208e588d581fbc30b6f2cb3d97c09e89d
xvrgill <60839017+xvrgill@users.noreply.github.com>
2022-02-17 11:48:19 -0500

    fixed context manager arguements

--- api/resources/pptx.py
+++ api/resources/pptx.py
@@ -54,7 +54,7 @@ class PPT(Resource):
         #! Development paths - not to be used in production
         if not path.isdir("api/power_points"):
             mkdir("api/power_points")
-        with PPTContextManager(prs, ppt_filename, asset_paths) as f:
+        with PPTContextManager(prs, ppt_filename) as f:
             # chdir("../power_points")
             f.save(f"api/power_points/{ppt_filename}")
             return send_file(f"power_points/{ppt_filename}", as_attachment=True, attachment_filename=ppt_filename)
